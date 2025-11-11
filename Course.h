1e43989843f9dca92d10580ad70581521695deb9
Ziad-Islam <ziadislamig@gmail.com>
2025-11-11 09:47:59 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -7,7 +7,43 @@ using namespace std;
 //#write your code here
 
 
-
+class Course {
+    private:
+        string courseCode;
+        string courseName;
+        int maxStudents;
+        Student* students;
+        int currentStudents;
+        
+    public:
+        void addStudent(const Student& s){
+            if(currentStudents == maxStudents){
+                cout << "Unable to add student." << endl;
+            } else {
+                students[currentStudents] = s;
+                currentStudents++;
+            }
+            
+        }
+        
+        void displayCourseInfo(){
+            cout << "Course: " << courseCode << " - " << courseName << endl;
+            cout << "Max Students: " << maxStudents << endl;
+            cout << "Currently Enrolled: ";
+            for(int i = 0; i<currentStudents; i++){
+                cout << students[i].getName() << " (ID: " << students[i].getID() << ") ";
+            }
+        }
+        
+        Course(){};
+        ~Course(){};
+        Course(string courseCode, string courseName, int maxStudents){
+            this->courseCode = courseCode;
+            this->courseName = courseName;
+            this->maxStudents = maxStudents;
+            currentStudents = 0;
+        }
+};
 
 
 
