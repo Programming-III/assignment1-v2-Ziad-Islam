c5d262531714c52cdc9cda5f05ef0f4def34611c
Ziad-Islam <ziadislamig@gmail.com>
2025-11-11 09:46:39 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -4,12 +4,29 @@
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+    private:
+        string name;
+        int id;
+    public:
+        void display(){
+            cout << "Name: " << name << endl << "ID: " << id << endl;
+        }
+        string getName(){
+            return name;
+        }
+        int getID(){
+            return id;
+        }
+        
+        Person(){};
+        ~Person(){};
+        Person(string name, int id){
+            this->name = name;
+            this->id = id;
+        }
 
-
-
-
-
+};
 
 
 
